a285a9ebb0409f2205b141edd6db71229304fa70
Nicolas Ballian <nicobal@gmail.com>
2019-10-10 20:21:32 -0300

    done without views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+[
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '41 31 24 62',
+    category:        'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '43 31 92 16',
+    category:        'belgian'
+  },
+  {
+    name:         'La Pizza',
+    address:      'rue claude bernard',
+    phone_number:  '43 31 92 16',
+    category:        'japanese'
+  },
+  {
+    name:         'Pizza East',
+    address:      'joaquim tavora',
+    phone_number:  '43 31 92 17',
+    category:        'italian'
+  },
+  {
+    name:         'panda',
+    address:      'eucaliptos',
+    phone_number:  '43 31 92 18',
+    category:        'french'
+  }
+]
